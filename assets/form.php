ced75a8536a5cb207481348c9f81dc3bfe1926a6
Victor Colomb <vic.col@hotmail.fr>
2022-02-16 02:25:15 +0100

    index.php

---
+++ assets/form.php
@@ -0,0 +1,156 @@
+<?php
+/*
+	********************************************************************************************
+	CONFIGURATION
+	********************************************************************************************
+*/
+// destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par un point-virgule
+$destinataire = 'victorcolomb@outlook.com;ugo.bonnet-lorente@student.isae-supaero.fr;bertille.martinier@student.isae-supaero.fr';
+
+// copie ? (envoie une copie au visiteur)
+$copie = 'oui'; // 'oui' ou 'non'
+
+// Messages de confirmation du mail
+$message_envoye = "Merci pour votre message. Nous y répondrons dans les plus brefs délais !";
+$message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer.";
+
+// Messages d'erreur du formulaire
+$message_erreur_formulaire = "Vous devez d'abord <a href=\"\contact\">envoyer le formulaire</a>.";
+$message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
+
+/*
+	********************************************************************************************
+	FIN DE LA CONFIGURATION
+	********************************************************************************************
+*/
+
+//Captcha
+
+// Ma clé privée
+$secret = "6LfC6qQZAAAAAC021QhQ82_mgkoExwI6aeoqrBd5";
+// Paramètre renvoyé par le recaptcha
+$response = $_POST['g-recaptcha-response'];
+// On récupère l'IP de l'utilisateur
+$remoteip = $_SERVER['REMOTE_ADDR'];
+
+$api_url = "https://www.google.com/recaptcha/api/siteverify?secret="
+	. $secret
+	. "&response=" . $response
+	. "&remoteip=" . $remoteip ;
+
+$decode = json_decode(file_get_contents($api_url), true);
+
+if ($decode['success'] == true) {
+	// C'est un humain
+	if (!isset($_POST['envoi']))
+	{
+		// formulaire non envoyé
+		echo '<p>'.$message_erreur_formulaire.'</p>'."\n";
+	}
+	else
+	{
+		/*
+		* cette fonction sert à nettoyer et enregistrer un texte
+		*/
+		function Rec($text)
+		{
+			$text = htmlspecialchars(trim($text), ENT_QUOTES);
+			if (1 === get_magic_quotes_gpc())
+			{
+				$text = stripslashes($text);
+			}
+
+			$text = nl2br($text);
+			return $text;
+		};
+
+		/*
+		* Cette fonction sert à vérifier la syntaxe d'un email
+		*/
+		function IsEmail($email)
+		{
+			$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
+			return (($value === 0) || ($value === false)) ? false : true;
+		}
+
+		// formulaire envoyé, on récupère tous les champs.
+		$nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
+		$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
+		$objet   = (isset($_POST['objet']))   ? Rec($_POST['objet'])   : '';
+		$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
+
+		// On va vérifier les variables et l'email ...
+		$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
+
+		if (($nom != '') && ($email != '') && ($objet != '') && ($message != ''))
+		{
+			// les 4 variables sont remplies, on génère puis envoie le mail
+			$headers  = 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'From:'.$nom.' <'.$email.'>' . "\r\n" .
+					'Reply-To:'.$email. "\r\n" .
+					'Content-Type: text/plain; charset="utf-8"; DelSp="Yes"; format=flowed '."\r\n" .
+					'Content-Disposition: inline'. "\r\n" .
+					'Content-Transfer-Encoding: 7bit'." \r\n" .
+					'X-Mailer:PHP/'.phpversion();
+
+			$message2 = "Votre message au BDE de Supaero : \r\n \r\n" ;
+			$message = $message2 . $message ;
+
+			// envoyer une copie au visiteur ?
+			if ($copie == 'oui')
+			{
+				$cible = $destinataire.';'.$email;
+			}
+			else
+			{
+				$cible = $destinataire;
+			};
+
+			// Remplacement de certains caractères spéciaux
+			$caracteres_speciaux     = array('&#039;', '&#8217;', '&quot;', '<br>', '<br />', '&lt;', '&gt;', '&amp;', '…',   '&rsquo;', '&lsquo;');
+			$caracteres_remplacement = array("'",      "'",        '"',      '',    '',       '<',    '>',    '&',     '...', '>>',      '<<'     );
+
+			$objet = "[ae-isae-supaero.fr] " . html_entity_decode($objet);
+			$objet = str_replace($caracteres_speciaux, $caracteres_remplacement, $objet);
+
+			$message = html_entity_decode($message);
+			$message = str_replace($caracteres_speciaux, $caracteres_remplacement, $message);
+
+			// Envoi du mail
+			$num_emails = 0;
+			$tmp = explode(';', $cible);
+			foreach($tmp as $email_destinataire)
+			{
+				if (mail($email_destinataire, $objet, $message, $headers))
+					$num_emails++;
+			}
+
+			if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
+			{
+				// echo '<p>'.$message_envoye.'</p>';
+				print ("<script language = \"JavaScript\">");
+				print ("location.href = '/contact-success';");
+				print ("</script>");
+			}
+			else
+			{
+				echo '<p>'.$message_non_envoye.'</p>';
+			};
+		}
+		else
+		{
+			// une des 3 variables (ou plus) est vide ...
+			echo '<p>'.$message_formulaire_invalide.' <a href="/contact">Retour au formulaire</a></p>'."\n";
+		};
+	}; // fin du if (!isset($_POST['envoi']))
+
+}
+else {
+	// C'est un robot ou le code de vérification est incorrecte
+	echo '<p>Une erreur est survenue, veuillez réessayer.</p>' ;
+} ;
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,166 @@
+<?php
+
+// AVAILABLE PAGES (allowed request => [html file to include, page title])
+// all html files have to be in the includes directory
+const PAGES = [
+  '/' => ['home.html', ''],
+  '/index.php' => ['home.html', ''],
+  '/home' => ['home.html', ''],
+  '/admissibles' => ['admissibles.html', 'Admissibles'],
+  '/alpha' => ['alpha.html', 'Video alpha'],
+  '/bde' => ['bde.html', 'Équipe BDE'],
+  '/campus' => ['campus.html', 'Campus'],
+  '/clubs' => ['clubs.html', 'Clubs'],
+  '/contact-success' => ['contact-success.html', 'Contactez nous'],
+  '/contact' => ['contact.html', 'Contactez nous'],
+  '/english' => ['english.html', ''],
+  '/galerie' => ['galerie.html', 'Galerie'],
+  '/mot-des-eleves' => ['mot-des-eleves.html', 'Mot des élèves'],
+  '/partenaires' => ['partenaires.html', 'Partenaires'],
+  '/prez-international' => ['prez-international.html', 'Mot du prez international'],
+  '/prez' => ['prez.html', 'Mot du prez'],
+  '/clubs/clubs-aa' => ['clubs-aa.html', 'Clubs AA'],
+  '/clubs/clubs-ae' => ['clubs-ae.html', 'Clubs AE'],
+  '/clubs/clubs-as' => ['clubs-as.html', 'Clubs AS'],
+  '/clubs/clubs-inde' => ['clubs-inde.html', 'Clubs indépendants'],
+];
+
+// PARSE REQUESTED PAGE (404 elsewise)
+$request_uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+if (array_key_exists($request_uri, PAGES)) {
+  $html = PAGES[$request_uri][0];
+  $page_title = PAGES[$request_uri][1];
+} else {
+  header('Location: /404.html');
+  exit();
+}
+$is_home = $html == 'home.html' || $html == 'english.html';
+
+?>
+
+<!doctype html>
+<html>
+
+<head>
+  <title>AE ISAE-SUPAERO <?php if (!empty($page_title)) {
+                            echo (' - ' . $page_title);
+                          } ?></title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" type="image/x-icon" href="favicon.ico" />
+
+  <!-- STYLESHEET -->
+  <?php if ($html == 'campus.html') { ?>
+    <link rel="stylesheet" href="/assets/css/campus.css" />
+  <?php } else if (in_array($html, ['clubs.html', 'clubs-aa.html', 'clubs-as.html', 'clubs-ae.html', 'clubs-inde.html'])) { ?>
+    <link rel="stylesheet" href="/assets/css/clubs.css" />
+  <?php } else { ?>
+    <link rel="stylesheet" href="/assets/css/main.css" />
+  <?php } ?>
+
+  <!-- GOOGLE ANALYTICS -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-EJRXPNP7PM"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-EJRXPNP7PM");
+  </script>
+  <script>
+    console.log("<?= $request_uri ?>")
+  </script>
+</head>
+
+<body <?php if ($is_home) {
+        echo 'class="landing"';
+      } ?>>
+
+  <!-- HEADER -->
+  <header id="header" <?php if ($is_home) {
+                        echo 'class="alt"';
+                      } ?>>
+    <h1>
+      <a href="/">Association des élèves <strong>ISAE-Supaero</strong></a>
+    </h1>
+    <nav id="nav">
+      <ul>
+        <li>
+          <a href="/admissibles"><span>Admissibles</span></a>
+        </li>
+        <li>
+          <a href="/clubs"><span>Clubs</span></a>
+        </li>
+        <li>
+          <a href="/campus"><span>Campus</span></a>
+        </li>
+        <li>
+          <a href="/galerie"><span>Galerie</span></a>
+        </li>
+        <li>
+          <a href="https://shop.ae-isae-supaero.fr">Boutique</a>
+        </li>
+        <li>
+          <a href="https://student.ae-isae-supaero.fr/"><span>Espace Adhérents</span></a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <!-- Menu display button for small screens -->
+  <a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+</body>
+
+<!-- BODY -->
+<?php include 'includes/' . $html; ?>
+
+<!-- FOOTER -->
+<footer id="footer">
+  <div class="container">
+    <ul>
+      <span class="logo">
+        <a href="bde">
+          <img src="/images/logo_fond_blanc_transparent_250.png" />
+        </a>
+      </span>
+    </ul>
+    <ul class="icons">
+      <li>
+        <a href="https://www.facebook.com/BDE.Supaero/" class="icon fa-facebook"></a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/bde_isae_supaero/" class="icon fa-instagram"></a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/company/bde-isae-supaero/" class="icon fa-linkedin"></a>
+      </li>
+    </ul>
+    <ul class="assos">
+      <li><a href="/contact">Nous contacter</a></li>
+      <li><a href="/partenaires">Nos partenaires</a></li>
+      <li><a href="https://www.as-isae-supaero.com/">Site de l'AS</a></li>
+      <li><a href="https://bda-isae-supaero.fr/">Site de l'AA</a></li>
+    </ul>
+    <ul class="copyright">
+      <li>
+        <p>&#169; 2020-<?php echo (date('Y')); ?> AE ISAE-SUPAERO</p>
+      </li>
+    </ul>
+  </div>
+  <div class="english">
+    <a href="./english" class="icon fa-globe"> English</a>
+  </div>
+</footer>
+
+<!-- SCRIPTS -->
+<script src="/assets/js/jquery.min.js"></script>
+<script src="/assets/js/skel.min.js"></script>
+<script src="/assets/js/util.js"></script>
+<script src="/assets/js/main.js"></script>
+<?php if ($is_home) {
+  echo ('<script src="/assets/js/first_visit.js"></script>');
+} ?>
+
+</html>
